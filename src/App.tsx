d570757e027f31e1923ae7250fdac8964cab0594
tonnysanjidaakter <tonnysanjidaakter@gmail.com>
2022-07-02 09:47:54 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+import Users from './components/Users';
+
+let student: string = 'Jeff';
+let age: number = 69;
+let isSmart: boolean = true;
+
+let students: string[] = ['Joe', 'Biden', 'Harris'];
+let fees: number[] = [12, 45, 78, 12, 45];
+
+interface Person {
+  name: string,
+  job?: string,
+  employed: string | boolean,
+  age: number,
+  location?: string | number
+}
+
+const person: Person = {
+  name: 'Bill Clinton',
+  employed: true,
+  age: 85,
+  location: 55
+}
+
+const handleAddUser = (firstName: string, age: number, address: string):number =>{
+    const total: number = 50;
+    console.log(firstName, age, address);
+    return age;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Counter></Counter>
+        <Users></Users>
     </div>
   );
 }
